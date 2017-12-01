92602aa6e816e7f719e0a6c1cba7f30caaacc2cc
kina <kina.girl@gmail.com>
2017-12-01 22:22:08 +0200

    initial commit

---
+++ src/aboutUs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class AboutUs extends  Component {
+  render () {
+    return (
+      <div>
+        <p>About us page</p>
+      </div>
+
+    )
+  }
+}
+
+export default AboutUs
